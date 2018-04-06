5432b1570552032b5daf340ea77a2167d02d81d9
devjin0617 <devjin0617@gmail.com>
2018-04-07 01:00:02 +0900

    카메라 진행중

--- components/Send.js
+++ components/Send.js
@@ -5,13 +5,18 @@ import {
   StyleSheet
 } from 'react-native';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
 export default class Send extends Component {
+  onSuccess(e) {
+    // Linking
+    //   .openURL(e.data)
+    //   .catch(err => console.error('An error occured', err));
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Send View!
-        </Text>
+        
       </View>
     );
   }
@@ -23,5 +28,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
-  }
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
 })

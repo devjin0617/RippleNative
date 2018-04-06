6bfb8167f8d214ca5a8348fc4f5d0e8e523bbb95
devjin0617 <devjin0617@gmail.com>
2018-04-06 22:09:06 +0900

    첫페이지 진행

--- components/StartPage.js
+++ components/StartPage.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { RkButton } from 'react-native-ui-kitten';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Button
 } from 'react-native';
 
-type Props = {};
-export default class StartPage extends Component<Props> {
+
+export default class StartPage extends Component {
+  createWallet () {
+    alert('createWallet')
+  }
+  openWallet () {
+    alert('openWallet')
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Hello RippleNative!
+        <Text style={styles.text}>
+          RippleNative (Ripple Wallet)
         </Text>
+        <RkButton onPress={this.createWallet} style={styles.button}>Create Wallet</RkButton>
+        <RkButton onPress={this.openWallet} style={styles.button}>Get the Wallet</RkButton>
       </View>
     );
   }
@@ -23,6 +33,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#324057',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white'
+  },
+  button: {
+    width: '90%',
+    marginTop: 10
   }
 })

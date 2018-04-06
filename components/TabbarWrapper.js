0ae9950e21a8eb7b5816d2f5fc2298045b0fa579
cheongjin.kim <jin@mealant.com>
2018-04-06 18:14:22 +0900

    탭바 이벤트 추가

--- components/TabbarWrapper.js
+++ components/TabbarWrapper.js
@@ -7,31 +7,54 @@ import {
 
 type Props = {};
 export default class TabbarWrapper extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'balance'
+    };
+  }
+  componentDidMount () {
+    this.setState({
+      selectedTab: 'history'
+    });
+  }
+  changeTab () {
+    this.setState({
+      selectedTab: 'history'
+    });
+  }
   render() {
     return (
-      <TabBarIOS>
+      <TabBarIOS selectedTab={this.state.selectedTab}>
         <TabBarIOS.Item
+          selected={this.state.selectedTab === 'balance'}
           title="잔액"
-          selected="true">
-
+          onPress={() => {
+            this.setState({
+              selectedTab: 'balance'
+            })
+          }}>
           <StartPage></StartPage>
-
         </TabBarIOS.Item>
-
         <TabBarIOS.Item
+          selected={this.state.selectedTab === 'send'}
           title="전송"
-          selected="false">
-
+          onPress={() => {
+            this.setState({
+              selectedTab: 'send'
+            })
+          }}>
           <View></View>
-
         </TabBarIOS.Item>
-        
         <TabBarIOS.Item
+          selected={this.state.selectedTab === 'history'}
           title="내역"
-          selected="false">
-          
+          onPress={() => {
+            this.setState({
+              selectedTab: 'history'
+            })
+          }}>
           <View></View>
-
         </TabBarIOS.Item>
       </TabBarIOS>
     )

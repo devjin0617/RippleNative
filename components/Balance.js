43f668fe156a1c402587ca051768edab834e26db
devjin0617 <devjin0617@gmail.com>
2018-04-06 23:58:16 +0900

    ripple-lib 추가 실패하여 관련코드 제거

--- components/Balance.js
+++ components/Balance.js
@@ -5,28 +5,11 @@ import {
   StyleSheet
 } from 'react-native';
 
-// import { RippleAPI } from 'ripple-lib';
-// import Ripple from 'ripple-lib';
-// const Ripple = require('ripple-lib');
+import Ripple from '../lib/ripple'
 
 export default class Balance extends Component {
-  componentDidMount () {
-    // const address = 'rMX46MC7EbTCfJZU6Yy61jwYbLnxvku2PM'
-    // const vm = this
-    // const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' })
-    // api.connect().then(() => {
-    //   api.getBalances(address).then(balances => {
-    //     vm.state = {
-    //       amount: balances[0].value
-    //     }
-    //   }).catch(err => {
-    //     if (err.name === 'RippledError') {
-          
-    //     } else {
-    //       alert('지갑정보를 불러오지 못했습니다.')
-    //     }
-    //   })
-    // })
+  async componentDidMount () {
+    const address = 'rMX46MC7EbTCfJZU6Yy61jwYbLnxvku2PM'
   }
   render() {
     return (
---
+++ memo.md
@@ -0,0 +1,12 @@
+### node모듈 사용시
+
+```
+$ npm i -g browserify
+$ npm i bufferutil
+$ npm i utf-8-validate
+$ browserify filename-in.js -o filename.js
+```
+
+```
+const FileName = require('./filename.js)
+```

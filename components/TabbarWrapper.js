f05003887b490350540e1cab0efbe20b7f73ba00
cheongjin.kim <jin@mealant.com>
2018-04-06 12:26:43 +0900

    오타 수정

--- components/TabbarWrapper.js
+++ components/TabbarWrapper.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
+import StartPage from './StartPage'
 import {
   TabBarIOS,
   View
 } from 'react-native';
 
 type Props = {};
-export default class TabbarWrapper extents Component<Props> {
+export default class TabbarWrapper extends Component<Props> {
   render() {
     return (
       <TabBarIOS>
         <TabBarIOS.Item
           title="잔액"
           selected="true">
-          <View></View>
+
+          <StartPage></StartPage>
+
         </TabBarIOS.Item>
+
         <TabBarIOS.Item
           title="전송"
           selected="false">
+
           <View></View>
+
         </TabBarIOS.Item>
+        
         <TabBarIOS.Item
           title="내역"
           selected="false">
+          
           <View></View>
+
         </TabBarIOS.Item>
       </TabBarIOS>
     )
